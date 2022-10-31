9140875f6557a4b7c45b5a127747af0cacccf000
windtalkerz <windtalkerz@users.noreply.github.com>
2022-10-31 10:47:25 +0000

    Initial Commit

---
+++ SalesmanApp/SalesmanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesmanAppApp.swift
+//  SalesmanApp
+//
+//  Created by marc on 31.10.22.
+//
+
+import SwiftUI
+
+@main
+struct SalesmanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
